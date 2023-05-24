37e7a74ca6ff2b3ccca6b9b5df4c268309d66ad4
benesjan <janbenes1234@gmail.com>
2023-05-24 18:09:34 +0200

    kind of finished

--- uniswap_portal.sol
+++ uniswap_portal.sol
@@ -1,37 +1,36 @@
 contract UniswapPortal {
-    public distributeProceeds() {
-        // We grab any message which can be consumed/whose swap is finalised
-        
+    IRollup constant ROLLUP = IRollup(0x1234);
+    IRegistry immutable registry;
+    bytes32 immutable l2ContractAddress;
+    IZKTokenPortal immutable zkTokenPortal;
 
-    let message: SwapResult = oracle.consume_l1_to_l2_message(content, msgKey, secret)
-    let token_address = message.succeeded ? message.token_address_b : message.token_address_a;
-    // This could be set by portal contract when emitting the message
-    let total_amount_out = message.succeeded ? message.amount_out : message.amount_in;
-    for (i in 0..deposits.len()) {
-      let deposit = deposits[i];
-      let amount_out = (deposit.amount / message.amount_in) * total_amount_out;
-      let note_hash = hash(amount_out, token_address, deposit.note_hash);
-      ctx.push_note_hash(note_hash);
+    constructor(bytes _l2ContractAddress, address _zkTokenPortalAddress) {
+        registry = IRegistry(ROLLUP.registry());
+        l2ContractAddress = _l2ContractAddress;
+        zkTokenPortal = IZKTokenPortal(_zkTokenPortalAddress);
+        IERC20(zkTokenPortal.underlying()).approve(address(zkTokenPortal), uint256(-1));
     }
-  }
 
-  public  claim() {
-    let claim_note = claims.at(call_context.msg_sender).get_1();
-    assert(claim_note);
-    claims.remove(claim_note);
-    IZkToken::approve();
-  }
+    public swap(uint256 _swapId, token_address_a, token_address_b, uint256[] _amounts, bytes32[] _partialNoteHashes) {
+        uint256 aggregatedAmount = 0;
+        for (uint256 i = 0; i < _amounts.length; i++) {
+            aggregatedAmount += _amounts[i];
+        }
+        DataStructures.L2ToL1Msg memory message = DataStructures.L2ToL1Msg({
+            sender: DataStructures.L2Actor(l2ContractAddress, 1),
+            recipient: DataStructures.L1Actor(address(this), 1),
+            content: Hash.sha256ToField(
+                abi.encodePacked([_swapId, token_address_a, token_address_b, _amounts, _partialNoteHashes])
+                )
+            });
+
+        bytes32 entryKey = registry.getOutbox().consume(message);
+
+        for (uint256 j = 0; j < _amounts; j++) {
+            zkTokenPortal.depositWithPartialNoteHash(_partialNoteHashes[j], _amounts[j]);
+        }
+    }
 }
-
-DataStructures.L2ToL1Msg memory message = DataStructures.L2ToL1Msg({
-      sender: DataStructures.L2Actor(aztecAddress, 1),
-      recipient: DataStructures.L1Actor(address(this), block.chainid),
-      content: Hash.sha256ToField(
-        abi.encodeWithSignature("withdraw(uint256,address)", _amount, _recipient)
-        )
-    });
-
-    bytes32 entryKey = registry.getOutbox().consume(message);
 
 contract ZkTokenPortal {
     constant IRollup ROLLUP = IRollup(0x1234);
@@ -42,18 +41,21 @@ contract ZkTokenPortal {
     }
 
   public depositWithPartialNoteHash(bytes32 _partialNoteHash, uint256 _amount) {
-    // Preamble
-    IInbox inbox = registry.getInbox();
-    DataStructures.L2Actor memory actor = DataStructures.L2Actor(l2TokenAddress, 1);
-
-    // Hash the message content to be reconstructed in the receiving contract
-    bytes32 content =
-      Hash.sha256ToField(abi.encodeWithSignature("mint(uint256,bytes32)", _amount, _partialNoteHash));
-
     // Hold the tokens in the portal
     underlying.safeTransferFrom(msg.sender, address(this), _amount);
 
+    // Hash the message content to be reconstructed in the receiving contract
+    bytes32 content = Hash.sha256ToField(abi.encodePacked([_amounts, _partialNoteHash]));
+    
+    sendMessageToRollup(content)
+  }
+
+  private sendMessageToRollup(bytes32 _content) {
+     // Preamble
+    IInbox inbox = registry.getInbox();
+    DataStructures.L2Actor memory actor = DataStructures.L2Actor(l2TokenAddress, 1);
+
     // Send message to rollup
-    return inbox.sendL2Message{value: msg.value}(actor, _deadline, content, _secretHash);
+    return inbox.sendL2Message{value: msg.value}(actor, _deadline, _content, _secretHash);
   }
 }
